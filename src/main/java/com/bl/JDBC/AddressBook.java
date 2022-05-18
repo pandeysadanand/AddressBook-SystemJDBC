90a4d349ba2ffc4de3eaaf256b9570b4db542d40
sadanand pandey <pandeysadanad75@gmail.com>
2022-05-18 07:01:24 +0530

    Added: edit contact

--- src/main/java/com/bl/JDBC/AddressBook.java
+++ src/main/java/com/bl/JDBC/AddressBook.java
@@ -74,4 +74,163 @@ public class AddressBook {
             throw new RuntimeException(e);
         }
     }
+
+    public void editContact(Connection con) throws Exception {
+        boolean change = true;
+        do {
+            System.out.println("What you want to change choose from below option:");
+            System.out.println("1.Last name\n2.Address\n3.City\n4.State\n5.zip\n6.Phone\n7.Email\n8.Close edit");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    editLastName(con);
+                    break;
+                case 2:
+                    editAddress(con);
+                    break;
+                case 3:
+                    editCity(con);
+                    break;
+                case 4:
+                    editState(con);
+                    break;
+                case 5:
+                    editPhone(con);
+                    break;
+                case 6:
+                    editEmail(con);
+                    break;
+                case 7:
+                    editZip(con);
+                    break;
+                default:
+                    change = false;
+                    System.out.println("Record edit closed !");
+            }
+        }while(change);
+    }
+
+
+    private void editZip(Connection con) throws Exception {
+        String query = "update contact_details set zip = ? where first_name= ?";
+        preparedStatement = con.prepareStatement(query);
+        System.out.println("Enter first name ");
+        String name = scanner.next();
+        System.out.println("Enter new email :");
+        int zip = scanner.nextInt();
+        preparedStatement.setInt(1, zip);
+        preparedStatement.setString(2, name);
+        int result = preparedStatement.executeUpdate();
+        if (result != 0) {
+            System.out.println("Record UPDATED successfully");
+        } else {
+            System.out.println("Record updated failed");
+        }
+    }
+
+    private void editEmail(Connection con) throws SQLException {
+        String query = "update contact_details set email = ? where first_name= ?";
+        preparedStatement = con.prepareStatement(query);
+        System.out.println("Enter first name ");
+        String name = scanner.next();
+        System.out.println("Enter new email :");
+        String email = scanner.next();
+        preparedStatement.setString(1, email);
+        preparedStatement.setString(2, name);
+        int result = preparedStatement.executeUpdate();
+        if (result != 0) {
+            System.out.println("Record UPDATED successfully");
+        } else {
+            System.out.println("Record updated failed");
+        }
+    }
+
+    private void editPhone(Connection con) throws Exception {
+        String query = "update contact_details set phone = ? where first_name= ?";
+        preparedStatement = con.prepareStatement(query);
+        System.out.println("Enter first name ");
+        String name = scanner.next();
+        System.out.println("Enter new phone number :");
+        int phone = scanner.nextInt();
+        preparedStatement.setInt(1, phone);
+        preparedStatement.setString(2, name);
+        int result = preparedStatement.executeUpdate();
+        if (result != 0) {
+            System.out.println("Record UPDATED successfully");
+        } else {
+            System.out.println("Record updated failed");
+        }
+    }
+
+
+    private void editState(Connection con) throws SQLException {
+        String query = "update contact_details set state = ? where first_name= ?";
+        preparedStatement = con.prepareStatement(query);
+        System.out.println("Enter first name ");
+        String name = scanner.next();
+        System.out.println("Enter new zip :");
+        String state = scanner.next();
+        preparedStatement.setString(1, state);
+        preparedStatement.setString(2, name);
+        int result = preparedStatement.executeUpdate();
+        if (result != 0) {
+            System.out.println("Record UPDATED successfully");
+        } else {
+            System.out.println("Record updated failed");
+        }
+    }
+
+
+    private void editCity(Connection con) throws Exception {
+        String query = "update contact_details set city = ? where first_name= ?";
+        preparedStatement = con.prepareStatement(query);
+        System.out.println("Enter first name ");
+        String name = scanner.next();
+        System.out.println("Enter new address :");
+        String city = scanner.next();
+        preparedStatement.setString(1, city);
+        preparedStatement.setString(2, name);
+        int result = preparedStatement.executeUpdate();
+        if (result != 0) {
+            System.out.println("Record UPDATED successfully");
+        } else {
+            System.out.println("Record update failed");
+        }
+    }
+
+
+    private void editAddress(Connection con) throws Exception {
+        String query = "update contact_details set address = ? where first_name= ?";
+        preparedStatement = con.prepareStatement(query);
+        System.out.println("Enter first name ");
+        String name = scanner.next();
+        System.out.println("Enter new address :");
+        String address = scanner.next();
+        preparedStatement.setString(1, address);
+        preparedStatement.setString(2, name);
+        int result = preparedStatement.executeUpdate();
+        if (result != 0) {
+            System.out.println("Record UPDATED successfully");
+        } else {
+            System.out.println("Record updated failed");
+        }
+    }
+
+
+    private void editLastName(Connection con) throws SQLException {
+        String query = "update contact_details set last_name = ? where first_name= ?";
+        preparedStatement = con.prepareStatement(query);
+        System.out.println("Enter first name ");
+        String name = scanner.next();
+        System.out.println("Enter new last name :");
+        String lname = scanner.next();
+        preparedStatement.setString(1, lname);
+        preparedStatement.setString(2, name);
+        int result = preparedStatement.executeUpdate();
+        if (result != 0) {
+            System.out.println("Record UPDATED successfully");
+        } else {
+            System.out.println("Record updated failed");
+        }
+    }
 }
